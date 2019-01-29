94db8b65b760140e743c4b043359e224bfe37304
Natalia Mutis <nataliamutiz@gmail.com>
2019-01-28 21:20:38 -0500

    Dibujando en canvas con eventos y formularios en html y javascript

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,27 +1,10 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorLinea);
+
 var dibujo = document.getElementById("dibujito");
 var lienzo = dibujo.getContext("2d");
-var lineas = 30
-var l = 0;
-var yi, xf, yi1, xf1,yi2;
-
-while(l < lineas) {
-  yi = 10 * l;
-  xf = 10 * (1 + l);
-  yi2 = 10 * (lineas - l);
-
-  dibujarLinea(0, yi, xf, 300, 'purple');
-  dibujarLinea(yi, 0, 300, xf, 'purple');
-  dibujarLinea(0, yi2, xf, 0, 'purple');
-  l = l + 1;
-  yi1 = 10 * (l - 1);
-  xf1 = 10 * (lineas - l);
-  dibujarLinea(300, yi1, xf1, 300, 'purple');
-}
-
-dibujarLinea(0,0, 300,0, 'purple');
-dibujarLinea(0,0, 0,300, 'purple');
-dibujarLinea(0,300, 300,300, 'purple');
-dibujarLinea(300,0, 300,300, 'purple');
+var ancho = dibujo.width;
 
 function dibujarLinea(xMoveTo, yMoveTo, xLineTo, yLineTo, color)
 {
@@ -31,4 +14,31 @@ function dibujarLinea(xMoveTo, yMoveTo, xLineTo, yLineTo, color)
   lienzo.lineTo (xLineTo, yLineTo);
   lienzo.stroke();
   lienzo.closePath();
+}
+
+function dibujoPorLinea()
+{
+  var lineas = parseInt(texto.value);
+  var espacio = ancho / lineas;
+  var l = 0;
+  var yi, xf, yi1, xf1,yi2;
+
+  while(l < lineas) {
+    yi = espacio * l;
+    xf = espacio * (1 + l);
+    yi2 = espacio * (lineas - l);
+
+    dibujarLinea(0, yi, xf, ancho, 'purple');
+    dibujarLinea(yi, 0, ancho, xf, 'purple');
+    dibujarLinea(0, yi2, xf, 0, 'purple');
+    l = l + 1;
+    yi1 = espacio * (l - 1);
+    xf1 = espacio * (lineas - l);
+    dibujarLinea(ancho, yi1, xf1, ancho, 'purple');
+  }
+
+  dibujarLinea(0,0, ancho,0, 'purple');
+  dibujarLinea(0,0, 0,ancho, 'purple');
+  dibujarLinea(0,ancho, ancho,ancho, 'purple');
+  dibujarLinea(ancho,0, ancho,ancho, 'purple');
 }
